f09dd7a72125639820a5c72e359c13d148ada873
Dreadblade-dev <sqandex1suggestions@gmail.com>
2020-06-13 13:11:51 +0200

    Added a JavaDoc

--- src/main/java/com/dreadblade/datastructures/stack/StackArray.java
+++ src/main/java/com/dreadblade/datastructures/stack/StackArray.java
@@ -36,6 +36,9 @@ public class StackArray<T> implements Stack<T> {
     private Object[] data;
     private static final int CAPACITY_VALUE = 16;
 
+    /**
+     * StackArray constructor
+     */
     public StackArray()
     {
         this.capacity = CAPACITY_VALUE;
@@ -43,6 +46,10 @@ public class StackArray<T> implements Stack<T> {
         this.data = new Object[capacity];
     }
 
+    /**
+     * StackArray constructor with capacity parameter
+     * @param capacity StackArray initial <code>int</code> capacity
+     */
     public StackArray(int capacity)
     {
         this.capacity = capacity;
@@ -50,11 +57,18 @@ public class StackArray<T> implements Stack<T> {
         this.data = new Object[capacity];
     }
 
+    /**
+     * @return true if StackArray is empty
+     */
     @Override
     public boolean isEmpty() {
         return (size == -1);
     }
 
+    /**
+     * Pushes item on top of the StackArray
+     * @param item <code>T item</code> to be pushed
+     */
     @Override
     public void push(T item) {
         if (size + 1 == capacity)
@@ -64,6 +78,10 @@ public class StackArray<T> implements Stack<T> {
         data[size] = item;
     }
 
+    /**
+     * Returns and delete item on top of the StackArray
+     * @return <code>T item</code> on top of the StackArray
+     */
     @Override
     public T pop() {
         if (isEmpty())
@@ -75,6 +93,10 @@ public class StackArray<T> implements Stack<T> {
         return (T) data[size + 1];
     }
 
+    /**
+     * Returns item on top of the StackArray
+     * @return <code>T item</code> on top of the StackArray
+     */
     @Override
     public T peek() {
         if (isEmpty())
@@ -83,20 +105,33 @@ public class StackArray<T> implements Stack<T> {
         return (T) data[size];
     }
 
+    /**
+     * @return StackArray <code>int size</code>
+     */
     @Override
     public int getSize() {
         return size + 1;
     }
 
+    /**
+     * Clears the StackArray
+     */
     @Override
     public void clear() {
         size = -1;
     }
 
+    /**
+     * @return StackArray <code>int capacity</code>
+     */
     public int getCapacity() {
         return capacity;
     }
 
+    /**
+     * Changes StackArray capacity to a new value
+     * @param capacity new StackArray <code>int capacity</code>
+     */
     public void changeCapacity(int capacity) {
         data = Arrays.copyOf(data, capacity);
         this.capacity = capacity;
