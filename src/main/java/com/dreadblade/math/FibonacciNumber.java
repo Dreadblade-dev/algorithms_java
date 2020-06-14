810189110fff10d8df8e4a8a1be66368cb081654
Dreadblade-dev <sqandex1suggestions@gmail.com>
2020-06-14 23:58:34 +0200

    Added a JavaDoc

--- src/main/java/com/dreadblade/math/FibonacciNumber.java
+++ src/main/java/com/dreadblade/math/FibonacciNumber.java
@@ -1,5 +1,21 @@
 package com.dreadblade.math;
 
+/**
+ * @author Dreadblade- (https://github.com/Dreadblade-dev)
+ * Fast Fibonacci algorithm realization
+ * Time complexity:
+ * O(n)
+ *
+ * In mathematics, the Fibonacci numbers, commonly denoted F(n),
+ * form a sequence, called the Fibonacci sequence, such that
+ * each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+ * F(0) = 0, F(1) = 1,
+ * and
+ * F(n) = F(n − 1) + F(n − 2),
+ * for n > 1.
+ * Source: https://en.wikipedia.org/wiki/Fibonacci_number
+ */
+
 public class FibonacciNumber {
     private static int[] array = new int[128];
 
